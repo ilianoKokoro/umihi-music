fda792ca69e5f10eca5edde54c80347e87ea69c8
ilianoKokoro <iliandelagrange@gmail.com>
2026-06-30 11:24:31 -0400

    revert android auto fix

--- app/src/main/java/ca/ilianokokoro/umihi/music/services/UmihiMediaLibraryCallback.kt
+++ app/src/main/java/ca/ilianokokoro/umihi/music/services/UmihiMediaLibraryCallback.kt
@@ -62,7 +62,7 @@ class UmihiMediaLibraryCallback(
                     .setMediaId(Constants.ExoPlayer.Library.ROOT_ID)
                     .setMediaMetadata(
                         MediaMetadata.Builder()
-                            .setIsBrowsable(true)
+                            .setIsBrowsable(false)
                             .setIsPlayable(false)
                             .setMediaType(MediaMetadata.MEDIA_TYPE_FOLDER_MIXED)
                             .build()
